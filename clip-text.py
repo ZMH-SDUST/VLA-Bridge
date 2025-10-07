724c2bbc04bc998a325a76df932328400cbcd281
ZMH-SDUST <zzousdust@gmail.com>
2025-10-07 21:37:31 +0100

    Zzou

---
+++ clip-text.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2024/7/26 16:21
+@Auther ： Zzou
+@File ：clip-text.py
+@IDE ：PyCharm
+@Motto ：ABC(Always Be Coding)
+@Info ：
+"""
+
+import torch
+import clip
+from PIL import Image
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+
+image = preprocess(Image.open("CLIP.png")).unsqueeze(0).to(device)
+text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
+
+with torch.no_grad():
+    image_features = model.encode_image(image)
+    text_features = model.encode_text(text)
+
+    logits_per_image, logits_per_text = model(image, text)
+    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+print("Label probs:", probs)
---
+++ visualization/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2024/10/22 8:18
+@Auther ： Zzou
+@File ：__init__.py.py
+@IDE ：PyCharm
+@Motto ：ABC(Always Be Coding)
+@Info ：
+"""
